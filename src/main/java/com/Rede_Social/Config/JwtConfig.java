c9285a7651cfbad0d95c7849255a7c039aa26316
Vtormacs <macskids98play@hotmail.com>
2024-11-24 09:58:11 -0300

    seila kkkkkkkk secreto

--- src/main/java/com/Rede_Social/Config/JwtConfig.java
+++ src/main/java/com/Rede_Social/Config/JwtConfig.java
@@ -1,12 +1,18 @@
 package com.Rede_Social.Config;
 
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtConfig {
 
-	//Parâmetros para geração do token
-	public static final String SECRET_KEY = "UMACHAVESECRETADASUAAPIAQUIUMACHAVESECRETADASUAAPIAQUIUMACHAVESECRETADASUAAPIAQUIUMACHAVESECRETADASUAAPIAQUI";
+	@Value("${HASH_TOKEN}")
+	private String secretKey;
 	public static final SignatureAlgorithm ALGORITMO_ASSINATURA = SignatureAlgorithm.HS256;
-	public static final int HORAS_EXPIRACAO_TOKEN = 1;
+	public static final int HORAS_EXPIRACAO_TOKEN = 3;
 
+	public String getSecretKey() {
+		return secretKey;
+	}
 }
